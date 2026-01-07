cfe14cc47f7cbb5829c325c1a3d647fd22dd37db
Chonthicha Hintao <Chonthicha.hi@kkumail.com>
2026-01-07 22:24:00 +0700

    Update root route to swagger

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
-const cors = require('cors');
-const helmet = require('helmet');
-const connectDB = require('./config/db');
-const { errorHandler } = require('./middleware/errorMiddleware');
-const swaggerUi = require('swagger-ui-express');
-const swaggerSpecs = require('./config/swagger');
+const express = require("express");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const helmet = require("helmet");
+const connectDB = require("./config/db");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpecs = require("./config/swagger");
 
 // Connect Database
 connectDB();
@@ -18,18 +18,20 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
 // Routes
-app.use('/api/customers', require('./routes/customerRoutes'));
-app.use('/api/orders', require('./routes/orderRoutes'));
-app.use('/api/transactions', require('./routes/transactionRoutes'));
+app.use("/api/customers", require("./routes/customerRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
+app.use("/api/transactions", require("./routes/transactionRoutes"));
 
 app.use(errorHandler);
 
+app.get("/", (req, res) => {
+  res.redirect("/api-docs/#/Orders/get_api_orders");
+});
 const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
 
 if (require.main === module) {
   app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
